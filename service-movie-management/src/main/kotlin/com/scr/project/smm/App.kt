1f8dc2c79dba174435194df135119593ec37a080
scroutchy <stef.crombez@gmail.com>
2025-02-26 13:30:07 -0300

    Initial commit

---
+++ service-movie-management/src/main/kotlin/com/scr/project/smm/App.kt
@@ -0,0 +1,11 @@
+package com.scr.project.smm
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class App
+
+fun main(args: Array<String>) {
+	runApplication<App>(*args)
+}
